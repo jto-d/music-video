21d895136b236fd7562c01d32d655ab3f8f9c114
Jack <90474780+jto-d@users.noreply.github.com>
2022-02-03 14:01:00 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -3,20 +3,15 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
-# query to be searched
+
 query = "yellow"
 URL = "https://www.google.com/search?q=" + query + "&rlz=1C1CHBF_enUS983US983&hl=en&tbm=isch&sxsrf=APq-WBuLfSOHmblmLB_DuKpFKoAdUarxvw:1643827311774&source=lnms&sa=X&ved=0ahUKEwiap_X31eH1AhX_kokEHSeDD00Q_AUIvw4oAQ&biw=1920&bih=969&dpr=1"
 
-# "get" the page from the URL
 page = requests.get(URL)
 
-# parse the page with bs
 soup = BeautifulSoup(page.content, 'html.parser')
-
-# find all images
 image_tags = soup.find_all('img')
 
-# pick a random image out of the first 5 images and get the link
 index = random.randrange(0,5)
 link = image_tags[index]['src']
 print(link)
