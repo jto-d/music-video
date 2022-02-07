2db6d86e0cc5765864fe1c2d9a5d5fd0e7984d9b
Evantl17 <82623080+Evantl17@users.noreply.github.com>
2022-02-07 13:44:09 -0500

    Add files via upload

--- lyrics.py
+++ lyrics.py
@@ -1,6 +1,37 @@
-from PyLyrics import *
+from bs4 import BeautifulSoup
+import requests
 
-albums = PyLyrics.getAlbums(singer='Eminem')
-myalbum = albums[4] #Select your album based on Index
 
-tracks = myalbum.tracks() #or PyLyrics.getTracks(myalbum)
+artist = input("Enter artist name (no spaces):")
+title = input("Enter song name (no spaces)")
+URL = "https://www.azlyrics.com/lyrics/" + artist + "/" + title + ".html"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+
+lyrics = soup.find_all('div', class_="col-xs-12 col-lg-8 text-center")
+lyrics = lyrics[0].find_all('div')[5].text
+print(lyrics)
+
+def convert(lst):
+    return (lst[0].split())
+ 
+# Driver code
+lst =  [lyrics]
+
+lst = convert(lst)
+signal = True
+final = []
+for el in lst:
+    if '(' not in el and signal and '[' not in el:
+        final.append(el)
+    elif '(' in el and signal:
+        signal = False
+    elif ')' in el:
+        signal=True
+
+
+print(final)
+
+
+#print(soup.prettify())
